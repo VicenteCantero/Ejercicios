0fbcbe0dac94f2f773cd52df5881f3687b228cf6
Vicente Cantero Luna <vicente.cantero@bosonit.com>
2022-08-12 08:59:11 +0200

    SA2 falta StorageProperties

---
+++ SA2/src/main/java/com/bosonit/SA2/storage/StorageFileNotFoundException.java
@@ -0,0 +1,8 @@
+package com.bosonit.SA2.storage;
+
+public class StorageFileNotFoundException extends ClassNotFoundException{
+
+    public StorageFileNotFoundException (String mensaje){
+        super(mensaje);
+    }
+}
